30eb7b2d63f8d7e89a1612a8c6e94e9379a6e62a
Matt Henderson <matt_henderson@sil.org>
2015-10-22 09:25:40 -0400

    Change batch script to use SourceTree's embedded sh.exe

--- find-branches_sourcetree-custom-action.bat
+++ find-branches_sourcetree-custom-action.bat
@@ -4,4 +4,4 @@ REM Change to this script's folder.
 cd %~dp0
 
 REM Run the shell script, passing it the arguments we received.
-"c:\Program Files (x86)\Git\bin\sh.exe" --login -i -c "find-branches_sourcetree-custom-action.sh \"%~1\" \"%~2\" \"%~3\" \"%~4\""
+"%LOCALAPPDATA%\Atlassian\SourceTree\git_local\bin\sh.exe" --login -i -c "find-branches_sourcetree-custom-action.sh \"%~1\" \"%~2\" \"%~3\" \"%~4\""
